bad0c368e7d3f1b97bc07469ebaa3478860309ab
Oscartorrepastor <oscartorrepastor@gmail.com>
2025-09-16 10:50:31 +0200

    summary

---
+++ Python/AcesoDatos-Ejercicios-1ª.py
@@ -0,0 +1,45 @@
+# Ejercicio 1 
+# Diferencias entre paradigma imperativo y declarativo
+
+# Imperativo: Indica paso a paso como de debe realizar una tarea
+
+# Declarativo: Describe el resultado que se quieres tener sin decir como hacerlo
+
+# Ejercicio 2 
+# Paradigma Imperativo
+
+# Para hacer un sandwich: 1º - Ir a la cocicna 2º - abrir el armario 
+# 3º - sacar el pan 4º - abrir la nevera 
+# 5º - sacar el jamon y el queso 6º - hacer el sandwich 
+# 7º - comer el sandwich
+
+# Ejercicio 3 
+# Paradigma Orientado a Objetos
+
+# Permite que el codigo sea reutilizado, organizado y sencillo de mantener 
+
+# Ejercicio 4 
+# Tipado Dinamico
+
+# Una variable no necesita ser declarada como un yipo expecifico al momnto de 
+# crearla, puede contener diferentes tipos de valores a lo largo de su vida.
+
+# Ejercicio 5 
+# Implementa los siguientes ejercicios en Python
+
+# 5.1 - Suma de números
+
+num1 = int(input("Introduce el primer número: "))
+num2 = int(input("Introduce el segundo número: "))
+suma = num1 + num2
+print ("La suma es:", suma)
+
+# 5.2 - Calculadora de area de un cuadrado
+
+lado = int(input("Introduce el lado del cuadrado: "))
+lado * lado
+print("El área del cuadrado es:", lado * lado)
+
+# 5.3 - Conversion de temperatura
+
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Oscartorrepastor-Oscartorrepastor_VSCODE
 Este repositorio solo se subirá VSCODE 
+Primer proyecto creado para practicar python y aprender desde 0
